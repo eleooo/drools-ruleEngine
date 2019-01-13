0be79e6aefe696fcb54f1480ec84147e1572773b
chenxbook <chenxbook@126.com>
2019-01-14 00:24:36 +0800

    测试反欺诈规则A001&A002

---
+++ drools-server/src/main/java/com/jy/modules/drools/entity/Applicant.java
@@ -0,0 +1,38 @@
+package com.jy.modules.drools.entity;
+
+import java.util.Date;
+
+/**
+ * Created by apple on 2019/1/13.
+ */
+public class Applicant {
+    private String  custName;
+
+    private String  cardId;
+
+    private Date  birthDate;
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
